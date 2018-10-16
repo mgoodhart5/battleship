a1eede7649ec248b4c1210a14e6d8f31140593b6
abenetka <ali.benetka@gmail.com>
2018-10-15 18:16:28 -0600

    Makes space methods, all tests passing

--- lib/space.rb
+++ lib/space.rb
@@ -1,25 +1,30 @@
 require './lib/ship'
 
 class Space
-  attr_reader :blank
+  attr_reader :status, :coordinate, :has_ship
 
-  def initialize
-    @blank = " "
+  def initialize(coordinate)
+    @status = " "
+    @coordinate = coordinate
+    @has_ship = false
   end
 
-  def miss
-    if @blank == true
-      @blank = "M"
+  def not_fired_upon
+    @status == " "
+  end
+
+  def fire_upon
+    #if theres a ship in the space, it needs to be H
+    # if theres no ship, it need to be M
+    if @has_ship == true
+      @status = "H"
+    else
+      @status = "M"
     end
+
   end
-#this can pretty much be one method
-  def hit
-    @blank = "H"
-    # we know that this has to be dependent
-    # on whether a ship is present or not
-  end
+
+
+
 
 end
-
-#way to load ship into space
-#ship class
--- test/space_test.rb
+++ test/space_test.rb
@@ -5,27 +5,27 @@ require './lib/ship'
 class SpaceTest < Minitest::Test
 
   def test_it_exists
-    space = Space.new
+    space = Space.new("A1")
     assert_instance_of Space, space
   end
 
   def test_it_is_default_blank_when_initialized
-    space = Space.new
-    assert_equal true, space.blank
+    space = Space.new("A1")
+    assert_equal true, space.not_fired_upon
   end
 
-  def test_that_blank_changes_to_M_when_missed
-    space = Space.new
-    assert_equal "M", space.miss
+  def test_it_is_initialized_with_a_coordinate
+    space = Space.new("A1")
+    assert_equal "A1", space.coordinate
   end
 
-  def test_that_space_can_change_to_H
-    space = Space.new
-    assert_equal "H", space.hit
-    #we know that ship will ultimately be an instance variable
+  def test_that_it_can_be_fired_upon_with_no_ship
+    space = Space.new("A1")
+    space.fire_upon
+    assert_equal "M", space.status
   end
 
-# work in progress
+  # def test
 
 
 end
