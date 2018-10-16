172212f0eefabc417d521064adc840f106942c98
abenetka <ali.benetka@gmail.com>
2018-10-16 14:55:56 -0600

    Modifies and refactors validation station

--- lib/board.rb
+++ lib/board.rb
@@ -26,21 +26,27 @@ class Board
   end
 
   def validation_station(little_ship_coords, big_ship_coords)
+    binding.pry
     overlap_station(little_ship_coords, big_ship_coords) == true
   end
 
 
-  def sanitation_station(coordinates, ship_size)
-    sani_coords = coordinates.upcase.split
-      if ship_size == 2
-        little_ship_coords = little_ship_validation(sani_coords)
-      else ship_size == 3
-        big_ship_coords = big_ship_validation(sani_coords)
-      end
+  def sanitation_station(placement_ship_1, placement_ship_2)
+    little_ship_coords = placement_ship_1.upcase
+    big_ship_coords = placement_ship_2.upcase
+    little_ship_validation(little_ship_coords)
+    big_ship_validation(big_ship_coords)
+    if little_ship_validation(little_ship_coords) && big_ship_validation(big_ship_coords)
+      overlap_station(little_ship_coords, big_ship_coords)
+    else
+      puts "invalid entry"
+    end
+
   end
 
 
   def little_ship_validation(little_ship_coords)
+    # binding.pry
     valid_little_ship = ["A1 A2", "A2 A3", "A3 A4", "B1 B2", "B2 B3", "B3 B4",
       "C1 C2", "C2 C3", "C3 C4", "D1 D2", "D2 D3", "D3 D4",
       "A1 B1", "B1 C1", "C1 D1", "A2 B2", "B2 C2", "C2 D2",
@@ -57,13 +63,15 @@ class Board
   end
 
   def overlap_station(little_ship_coords, big_ship_coords)
-    if little_ship_validation(little_ship_coords) && big_ship_validation(big_ship_coords) == true
-      ship_array = little_ship_coords.split + big_ship_coords.split
-      final_ship_array = ship_array.uniq
-      ship_array.length == final_ship_array.length
-      #loop life with if statement
+    ship_array = little_ship_coords.split + big_ship_coords.split
+    final_ship_array = ship_array.uniq
+    if ship_array.length == final_ship_array.length
+      puts "Place ships"
+    else
+      puts "WRONG"
     end
   end
+
 
 
 # method that checks if the first and last coordinate are equal to ship's size
