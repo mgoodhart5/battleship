4423f92502d6f997dccc668ca107009def7e7ae5
abenetka <ali.benetka@gmail.com>
2018-10-15 18:27:33 -0600

    Modifies ship class, tests passing

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -4,13 +4,15 @@ require './lib/ship'
 class ShipTest < Minitest::Test
 
   def test_that_it_exists
-    ship = Ship.new
+    ship = Ship.new("A1 A2")
     assert_instance_of Ship, ship
   end
 
-  def test_that_it_has_size
-    ship = Ship.new
-    assert_equal 1, ship.size
+  def test_it_can_be_initialized_with_coordinates
+    ship = Ship.new("A1 A2")
+    assert_equal "A1 A2", ship.location
   end
+
+
 
 end
